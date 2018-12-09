3af35cd9d1d6ce3d389ab37c18016bbdc5e93382
Seyoon <rlatpdbs@gmail.com>
2018-12-10 00:20:47 +0900

    2.1 Apollo Setup part One

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { ApolloProvider } from 'react-apollo'
+import client from './apollo'
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root') as HTMLElement
 );
